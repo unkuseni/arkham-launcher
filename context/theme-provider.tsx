b27e0e3000b91edabb3931d908fd016356b76200
unkuseni <amudalatadeoye@gmail.com>
2025-05-04 14:24:30 +0000

    feat: enhance ThemeProviderWrapper documentation and add missing dependency

--- context/theme-provider.tsx
+++ context/theme-provider.tsx
@@ -1,9 +1,25 @@
-"use client";
 import { ThemeProvider } from "next-themes";
 
+/**
+ * A wrapper around the `ThemeProvider` component from `next-themes`.
+ *
+ * The `ThemeProvider` component is used to manage the theme of the application.
+ * It provides a default theme and allows the user to switch between different
+ * themes.
+ *
+ * The `attribute` prop is set to `"class"` to add the theme to the `class` attribute
+ * of the `html` element.
+ *
+ * The `defaultTheme` prop is set to `"system"` to use the system theme by default.
+ * The `enableSystem` prop is set to `true` to allow the user to switch between
+ * different themes.
+ */
 export function ThemeProviderWrapper({
 	children,
 }: {
+	/**
+	 * The children to be rendered within the `ThemeProvider`.
+	 */
 	children: React.ReactNode;
 }) {
 	return (
