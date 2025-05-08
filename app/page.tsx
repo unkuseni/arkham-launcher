db9fd036106491486344ffd24d22fa49fb2f868f
unkuseni <amudalatadeoye@gmail.com>
2025-05-08 15:47:18 +0000

    feat: add policy signing functionality with terms agreement in Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 import { Header } from "@/components/header";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { type PolicyAgreement, signPolicy } from "@/lib/policy";
+import useUmiStore from "@/store/useUmiStore";
+import { set } from "@metaplex-foundation/umi/serializers";
+import { useWallet } from "@solana/wallet-adapter-react";
 import { ChartAreaIcon, Link as LinkIcon, Shield, Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useCallback, useState } from "react";
 import CountUp from "react-countup";
 
 export default function Home() {
@@ -22,6 +29,29 @@ export default function Home() {
 }
 
 const Hero = () => {
+	const [checked, setChecked] = useState(false);
+	const signer = useUmiStore((state) => state.signer?.publicKey) as string;
+
+	const handleSign = useCallback(async () => {
+		if (!signer) {
+			alert("Please connect your wallet first.");
+			return;
+		}
+
+		const policy: PolicyAgreement = {
+			user: signer,
+			policyId: "Policy-v1",
+			timestamp: Date.now(),
+			terms:
+				"I agree to the Arkham Launchpad Terms of service & Privacy Policy.",
+		};
+
+		const [signature, timestamp] = await signPolicy(policy);
+		console.log("Signature:", signature);
+		console.log("Timestamp:", timestamp);
+		console.log("Policy signed successfully!");
+	}, [signer]);
+
 	return (
 		<div className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center space-y-8 py-12 px-4 bg-[url('/hero-bg.jpg')] bg-cover bg-center bg-no-repeat rounded-3xl text-white">
 			<h1 className="text-center leading-relaxed text-2xl md:text-3xl lg:text-4xl font-bold font-inter bg-clip-text text-transparent bg-gradient-to-r from-gray-50 to-gray-600">
@@ -48,13 +78,26 @@ const Hero = () => {
 					</li>
 				</ul>
 			</div>
-			<div>
-				<Button
-					onClick={() => {
-						alert("coming soon");
-					}}
-				>
-					Get Started
+			<div className="flex flex-col items-center justify-center gap-4 w-full max-w-xl mx-auto">
+				<div className="flex items-center gap-2 font-sans">
+					<Checkbox
+						id="terms"
+						checked={checked}
+						onClick={() => setChecked(!checked)}
+					/>
+					<Label htmlFor="terms">
+						I agree to the{" "}
+						<Link href="#" className="text-blue-500 hover:text-blue-400">
+							Terms of Service
+						</Link>{" "}
+						and{" "}
+						<Link href="#" className="text-blue-500 hover:text-blue-400">
+							Privacy Policy
+						</Link>
+					</Label>
+				</div>
+				<Button onClick={handleSign} disabled={!checked}>
+					Launch Your x1000 Token
 				</Button>
 			</div>
 		</div>
