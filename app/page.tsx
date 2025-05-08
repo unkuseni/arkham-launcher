a648af9866cbb15746ab47b81a5a2a9e7fd6a2f2
unkuseni <amudalatadeoye@gmail.com>
2025-05-08 15:08:32 +0000

    feat: implement Hero section with token management features and update Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,111 +1,62 @@
 "use client";
-import { ThemeSwitcher } from "@/components/ui/mode-toggle";
+import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button";
+import { ChartAreaIcon, Link as LinkIcon, Shield, Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import CountUp from "react-countup";
 
 export default function Home() {
 	return (
-		<main
-			className={
-				"min-h-screen px-4 py-12 flex flex-col items-center justify-center transition-colors duration-300 bg-blue-300 dark:bg-gray-900"
-			}
-		>
-			<div className="fixed top-4 right-4">
-				<ThemeSwitcher />
-			</div>
-
-			<section className="max-w-6xl w-full flex flex-col-reverse md:flex-row items-center justify-between gap-10 mt-6">
-				{/* Left content */}
-				<div className="flex-1 text-center md:text-left">
-					<h1 className="text-3xl md:text-5xl font-bold mb-4">
-						Tokens & NFT with Ease
-					</h1>
-					<p className={"text-lg mb-6 dark:text-gray-300 text-gray-600 dark"}>
-						Launch Token, Liquidity, Airdrops and much more. Effortless and
-						without coding.
-					</p>
-					<div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
-						<Link
-							href={"/dashboard"}
-							className="bg-blue-600 text-white font-semibold px-6 py-3 rounded-xl hover:opacity-90 transition"
-						>
-							Create Launch
-						</Link>
-						<Link
-							href={"/dashboard"}
-							className="border border-blue-600 text-blue-600 font-semibold px-6 py-3 rounded-xl hover:bg-blue-100 hover:dark:bg-blue-800 transition"
-						>
-							Create Token
-						</Link>
-					</div>
-				</div>
-
-				{/* Right image */}
-				<div className="flex-1 flex justify-center">
-					<Image
-						src="/bg.webp"
-						alt="BGlogo"
-						width={320}
-						height={320}
-						className="w-64 md:w-80 h-auto"
-						priority
-					/>
-				</div>
-			</section>
-
-			{/* Stats */}
-			<section className="mt-16 text-center w-full max-w-6xl px-4">
-				<h2 className={"text-lg mb-6  dark:text-gray-400 text-gray-800"}>
-					OUR NUMBERS
-				</h2>
-
-				<div className="grid grid-cols-2 md:grid-cols-4 gap-6 border-t border-b border-gray-700 py-6">
-					{[
-						[2500, "PROJECTS"],
-						[7000, "USERS"],
-						[65000000, "VOLUME"],
-						[120000, "TRANSACTIONS"],
-					].map(([value, label], index) => {
-						const isLastColumnMd = index % 4 === 3;
-						const isLastColumnSm = index % 2 === 0;
-
-						return (
-							<div
-								key={label}
-								className={`relative flex flex-col items-center justify-center px-4
-            border-gray-500
-            ${index % 2 === 0 ? "border-r" : ""}
-            md:${index % 4 === 3 ? "border-r-0" : "border-r"}
-          `}
-							>
-								<p className="text-3xl font-extrabold dark:text-white">
-									+
-									<CountUp
-										end={Number(value)}
-										duration={2}
-										separator=","
-										enableScrollSpy
-										scrollSpyOnce
-										formattingFn={(num) => {
-											if (num >= 1_000_000) {
-												return `${(num / 1_000_000).toFixed(1).replace(/\.0$/, "")}M`;
-											}
-											if (num >= 1_000) {
-												return `${(num / 1_000).toFixed(1).replace(/\.0$/, "")}k`;
-											}
-											return num.toString();
-										}}
-									/>
-								</p>
-								<p className="text-sm tracking-widest mt-1 dark:text-white">
-									{label}
-								</p>
-							</div>
-						);
-					})}
-				</div>
-			</section>
-		</main>
+		<>
+			<Header />
+			<main
+				className={
+					"min-h-[calc(100vh-96px)] flex flex-col items-center justify-center transition-colors duration-300 px-4 py-2.5 md:px-6 md:py-4 xl:px-8 xl:py-6 "
+				}
+			>
+				<Hero />
+			</main>
+		</>
 	);
 }
+
+const Hero = () => {
+	return (
+		<div className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center space-y-8 py-12 px-4 bg-[url('/hero-bg.jpg')] bg-cover bg-center bg-no-repeat rounded-3xl text-white">
+			<h1 className="text-center leading-relaxed text-2xl md:text-3xl lg:text-4xl font-bold font-inter bg-clip-text text-transparent bg-gradient-to-r from-gray-50 to-gray-600">
+				Launch and manage your tokens <br />
+				in <span className="animate-pulse capitalize">seconds</span>
+			</h1>
+			<div className="flex items-center gap-2 w-full p-4">
+				<ul className="list-none flex flex-col w-full max-w-xl mx-auto font-bold font-sans tracking-wider">
+					<li className="h-16 md:self-start flex gap-3">
+						<Star />
+						<span>No-Code Token Wizard</span>
+					</li>
+					<li className="h-16 md:self-end flex gap-3">
+						<LinkIcon />
+						<span>Multichain Deployment</span>
+					</li>
+					<li className="h-16 md:self-start flex gap-3">
+						<Shield />
+						<span>Secure and Audited Contracts</span>
+					</li>
+					<li className="h-16 md:self-end flex gap-3">
+						<ChartAreaIcon />
+						<span>Live Dashboard and Analytics</span>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<Button
+					onClick={() => {
+						alert("coming soon");
+					}}
+				>
+					Get Started
+				</Button>
+			</div>
+		</div>
+	);
+};
