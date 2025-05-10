aabdd22297e5b2d2064f223a3a6899395ba8bf7f
unkuseni <amudalatadeoye@gmail.com>
2025-05-10 13:12:40 +0000

    feat: enhance DashboardHeader to dynamically generate breadcrumbs based on pathname

--- components/header/dashboard-header.tsx
+++ components/header/dashboard-header.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { Separator } from "@/components/ui/separator";
+import { capitalize } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 import {
 	Breadcrumb,
 	BreadcrumbItem,
@@ -10,6 +13,10 @@ import {
 import { SidebarTrigger } from "../ui/sidebar";
 
 const DashboardHeader = () => {
+	const pathname = usePathname();
+	const pathSegments = pathname.split("/").filter((segment) => segment);
+
+	console.log(pathname);
 	return (
 		<header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
 			<div className="flex items-center gap-2 px-4">
@@ -17,15 +24,27 @@ const DashboardHeader = () => {
 				<Separator orientation="vertical" className="mr-2 h-4" />
 				<Breadcrumb>
 					<BreadcrumbList>
-						<BreadcrumbItem className="hidden md:block">
-							<BreadcrumbLink href="#">
-								Building Your Application
-							</BreadcrumbLink>
-						</BreadcrumbItem>
-						<BreadcrumbSeparator className="hidden md:block" />
-						<BreadcrumbItem>
-							<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-						</BreadcrumbItem>
+						{pathSegments.map((segment, index) => {
+							const href = `/${pathSegments.slice(0, index + 1).join("/")}`;
+							const isLast = index === pathSegments.length - 1;
+							const displayName = capitalize(segment.replace(/-/g, " "));
+							return (
+								<BreadcrumbItem key={href}>
+									{isLast ? (
+										<BreadcrumbPage className="font-bold font-inter">
+											{displayName}
+										</BreadcrumbPage>
+									) : (
+										<BreadcrumbLink href={href} className="font-inter">
+											{displayName}
+										</BreadcrumbLink>
+									)}
+									{!isLast && (
+										<BreadcrumbSeparator className="hidden md:block mx-2 h-4" />
+									)}
+								</BreadcrumbItem>
+							);
+						})}
 					</BreadcrumbList>
 				</Breadcrumb>
 			</div>
